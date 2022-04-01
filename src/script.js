c06b4676619504852b41c143cf1bbf253c44e8ec
Netanel Mizrahi <nmiz1987@gmail.com>
2022-04-01 12:28:45 +0300

    fix bugs

--- src/script.js
+++ src/script.js
@@ -1,14 +1,21 @@
-var boxes = document.querySelectorAll('.box'); // all the boxes in upper part
-var ansBoxes = document.querySelectorAll('.box_ans'); // all the boxes in downer part
-var closeWinRes = document.querySelector('#symbol');
-var openWinRes = document.querySelector('#statsButton');
-var resultWindow = document.querySelector('.result-window');
+const boxes = document.querySelectorAll('.box'); // all the boxes in upper part
+const ansBoxes = document.querySelectorAll('.box_ans'); // all the boxes in downer part
+const closeWinRes = document.querySelector('#symbol');
+const openWinRes = document.querySelector('#statsButton');
+const resultWindow = document.querySelector('.result-window');
+var countPlayed = document.querySelector('#count-played');
+var countWon = document.querySelector('#count-Won');
+var countWin = document.querySelector('#count-Win');
 
 var answerRequired = ["8", "-", "0", "-", "3", "/", "1"]
 var resultRequired = 5
 var col = 0 // location in column
 var activeRow = 0 // location in row
-var showNext = 1 // to reveal next row
+var showNext = 0 // to reveal next row
+var correctAns = 0
+countPlayed.innerText = 0
+countWon.innerText = 0
+countWin.innerText = 0
 
 var currentBox = boxes[col + 7 * activeRow]
 
@@ -38,38 +45,52 @@ function userInput(e) {
             var temp = []
             for (let i = 0; i < 7; i++)
                 temp.push(boxes[i + 7 * activeRow].innerText)
-            try {
-                if (eval(temp.join('')) != resultRequired) {
-                    console.log("Wrong answer!")
-                } else {
-                    console.log("Correct!")
-                    let show = document.querySelector('#a_' + showNext++)
-                    show.setAttribute('style', 'display: block;')
-                    for (let i = 0; i < 7; i++) {
-                        for (let j = 0; j < 7; j++)
-                            if (Object.is(answerRequired[j], boxes[i + 7 * activeRow].innerText)) {
-                                if (i == j) {
-                                    boxes[i + 7 * activeRow].setAttribute('class', 'box filled current')
-                                } else {
-                                    if (Object.is(boxes[i + 7 * activeRow].getAttribute('class'), 'box filled current'))
-                                        continue
-                                    boxes[i + 7 * activeRow].setAttribute('class', 'box filled wrongPlace')
-                                }
-                            }
-                    }
-                    activeRow++
-                    col = 0
-                    currentBox = boxes[col + 7 * activeRow]
+                // try {
+            if (eval(temp.join('')) != resultRequired) {
+                console.log("Wrong answer!")
+            } else {
+                console.log("Correct!")
+                correctAns++
+                showNext++
+                // finished game
+                if (correctAns == 6) {
+                    console.log("Game finished!")
+                    countWon.innerText = Number(countWon.innerText) + 1
+                    resultWindow.style.display = "flex;";
+                    // show.setAttribute('style', 'display: flex;')
                 }
-            } catch {
-                console.log("invalid input")
+
+
+                let show = document.querySelector('#a_' + showNext)
+                show.setAttribute('style', 'display: block;')
+                for (let i = 0; i < 7; i++) {
+                    for (let j = 0; j < 7; j++)
+                        if (Object.is(answerRequired[j], boxes[i + 7 * activeRow].innerText)) {
+                            if (i == j) {
+                                boxes[i + 7 * activeRow].setAttribute('class', 'box filled current')
+                            } else {
+                                if (Object.is(boxes[i + 7 * activeRow].getAttribute('class'), 'box filled current'))
+                                    continue
+                                boxes[i + 7 * activeRow].setAttribute('class', 'box filled wrongPlace')
+                            }
+                        }
+                }
+
+
+                activeRow++
+                col = 0
+                currentBox = boxes[col + 7 * activeRow]
             }
+            // } catch {
+            // console.log("invalid input")
+            // }
 
         }
         if ((col % 7 == 0) && !(Object.is(e.code, 'Enter')) && !(keyButtonEnter == 'Enter')) { // don't move to next row
             col--
             currentBox = boxes[col + 7 * activeRow]
         }
+
     }
 
     if ((Object.is(e.code, 'Backspace') || keyButtonBackspace == 'Backspace') && col > 0) { // clear box
@@ -87,12 +108,10 @@ function userInput(e) {
 }
 
 closeWinRes.addEventListener('click', () => {
-    console.log("close")
     resultWindow.style.display = "none";
 })
 
 openWinRes.addEventListener('click', () => {
-    console.log("show")
     resultWindow.style.display = "flex";
 })
 
